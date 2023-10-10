3fce1e5871d776c3b03bcbc3d408f7331b38d15f
Christian Moyes <moyechri846@gmail.com>
2023-10-10 10:45:33 -0400

    part 2 done

--- Program.cs
+++ Program.cs
@@ -10,12 +10,12 @@
 
             while (choice != "q")
             {
-                Console.Clear(); // Optional
+                Console.Clear();
                 Console.WriteLine("Please select one of the demos:");
                 Console.WriteLine();
                 Console.WriteLine("1 - Prompter");
                 Console.WriteLine("2 - PercentPassing");
-                Console.WriteLine("3 - Menu Option x");
+                Console.WriteLine("3 - OddSum");
                 Console.WriteLine("Q - Quit");
                 Console.WriteLine();
                 choice = Console.ReadLine().ToLower().Trim();
@@ -35,7 +35,18 @@
                     Console.ReadLine();
                     PercentPassing();
                 }
-                
+                else if (choice == "3")
+                {
+                    Console.WriteLine("You chose the OddSum demo");
+                    Console.WriteLine("Hit ENTER to continue.");
+                    Console.ReadLine();
+                    OddSum();
+                }
+                else if (choice == "q")
+                {
+                    Console.WriteLine("Okay, I'll see you later!");
+                    Environment.Exit(0);
+                }
                 else
                 {
                     Console.WriteLine("Invalid choice, press ENTER to try again.");
@@ -76,8 +87,12 @@
 
         public static void PercentPassing()
         {
-            int grade, below70, above70, gradeNum;
+            double grade, below70, above70, gradeNum;
             bool done = false;
+            string answer;
+            below70 = 0;
+            above70 = 0;
+            gradeNum = 0;
             Console.WriteLine("Welcome to the PercentPassing demo!");
             Console.WriteLine("Give me some percentile grades, and I'll tell you what percent of them are equal to or above 70");
 
@@ -85,14 +100,61 @@
             {
                 Console.WriteLine("Give me a percentile grade between 0 and 100. (Don't include the %)");
 
-                while(!Int32.TryParse(Console.ReadLine(), out grade) || grade > 100 || grade < 0)
+                while(!Double.TryParse(Console.ReadLine(), out grade) || grade > 100 || grade < 0)
                 {
-
+                    Console.WriteLine("I'm sorry, that's not a valid input. Please ensure your inputted grade is between 0 and 100");
                 }
 
+                gradeNum++;
+                Math.Round(grade, 2);
+                Console.WriteLine($"Your inputted grade is {grade}%");
+
+                if (grade >= 70)
+                {
+                    above70++;
+                }
+                else
+                {
+                    below70++;
+                }
+
+                Console.WriteLine("Would you like to input another grade? (Yes/No)");
+                
+                answer = Console.ReadLine().ToUpper().Trim();
+
+                while (answer != "YES" && answer != "NO")
+                {
+                    Console.WriteLine("That is not a valid response, please try again.");
+                    answer = Console.ReadLine().ToUpper().Trim();
+                }
+
+                if (answer == "YES")
+                {
+                    Console.WriteLine("Okay.");
+                }
+                else if (answer == "NO")
+                {
+                    Console.WriteLine("Okay");
+                    Thread.Sleep(1250);
+                    done = true;
+                }
+                else
+                {
+                    Console.WriteLine("How did this happen?");
+                    Environment.Exit(0);
+                }
             }
+            
+            Console.WriteLine($"Out of {gradeNum} grades, {above70} were above or equal to 70%, and {below70} were below 70%.");
+            Console.WriteLine($"That means {Math.Round(above70/gradeNum*100), 2}% of them were above 70%");
+            Console.WriteLine("Press ENTER to exit this demo");
+            Console.ReadLine();
+
         }
 
+        public static void OddSum()
+        {
 
+        }
     }
 }
